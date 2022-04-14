39d4ccbc91839a3f279450782f96ab9580e95917
ange0223 <ahall@eventpower.com>
2022-04-13 20:09:20 -0400

    showing unasked questions only and shuffling answers

--- TriviaGame/TriviaGame/TriviaGame.cpp
+++ TriviaGame/TriviaGame/TriviaGame.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
+#include <random>
+#include <chrono>
 #include "TriviaHeader.hpp"
 
 using namespace std;
@@ -118,34 +121,64 @@ bool PlayerAlive () {
     return alive;
 };
 
+void resetQuestionsAsked(int current_cat){
+    for (int i = 0; i < cats[current_cat].questions.size(); i++) {
+        cats[current_cat].questions[i].asked = false;
+    }
+}
+
 bool AskQuestion(int current_cat){
     // Asks player question
-    int rand = 0;  // TODO: Feed rand to pick random question where asked = false
+    int rand = 0;
+    bool found_question = false;
 //    int answer_rand;
     int selected_answer;
     bool correct;
     vector<string> options;
+    
+    // Get next un-asked question
+    for (int i = 0; i < cats[current_cat].questions.size(); i++) {
+        if(cats[current_cat].questions[i].asked == false) {
+            rand = i;
+            found_question = true;
+            break;
+        }
+    }
+    
+    if (not(found_question)) {
+        // Run out of questions so repeating questions
+        cout << " This must be your favorite category, now we will be repeating questions!" << endl;
+        // Since all have been asked reseting
+        resetQuestionsAsked(current_cat);
+    }
     options.push_back(cats[current_cat].questions[rand].correct_answer);
     options.push_back(cats[current_cat].questions[rand].answer2);
     options.push_back(cats[current_cat].questions[rand].answer3);
     options.push_back(cats[current_cat].questions[rand].answer4);
-    // TODO: Ask question with answers shuffled
+    
+    // Actually asking question
     cout << cats[current_cat].questions[rand].question << endl;
     
+    // Shuffle Answers
+    auto random = default_random_engine {};
+    random.seed(std::chrono::system_clock::now().time_since_epoch().count());
+    shuffle(begin(options), end(options), random);
+    
+    // Displaying Options
     for (int i = 0; i < options.size(); i++) {
         cout << to_string(i) << ": " << options[i] << endl;
     };
     cout << "Please select an answer by number: " << endl;
     cin >> selected_answer;
-    
+        
     if (options[selected_answer] == cats[current_cat].questions[rand].correct_answer ) {
         correct = true;
     } else {
-        correct = false;
+            correct = false;
     }
-    
+        
     cats[current_cat].questions[rand].asked = true;
-    
+        
     return correct;
 }
 
@@ -163,6 +196,7 @@ void HandleAnswer(bool correct, Player &player) {
 
 void FinalGameCalculations(){
     // TODO: End Game, declare winner, state final score, record high score if necessary
+    cout << "End of Game" << endl;
 }
 void CheckIFHighScoreIsBeaten(int end_game_score){
 //this function will open and read the highscore.txt file and see if the score at the end of the game is higher than it if it is go to the
@@ -179,10 +213,11 @@ void PlayGame() {
         for(int i = 0; i < g.number_of_players; i++){
             // Goes through each player and asks question untill they lose 3 lives than goes to next player
             if (game_players.at(i).lives > 0) {
+                cout << endl;
+                PrintCategories();
                 cout << game_players.at(i).name << " please choose a category by number: " << endl;
                 bool answered_correct = AskQuestion(ChooseCategories()); // Asks Player to choose a category and than pulls a random question
                 HandleAnswer(answered_correct, game_players[i]);
-                
             }
         }
     }
@@ -195,7 +230,6 @@ void StartGame(int players){
     CreateCategories();
     CreateQuestions();
     AddPlayers();
-    PrintCategories();
     PlayGame();
 }
 
