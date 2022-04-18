e39250eb9c9f7733df829ca13c056fb1a3db470e
ange0223 <ahall@eventpower.com>
2022-04-18 13:52:53 -0400

    cleaning up a little and adding additional comments

--- TriviaGame/TriviaGame/TriviaGame.cpp
+++ TriviaGame/TriviaGame/TriviaGame.cpp
@@ -2,7 +2,59 @@
 //  TriviaGame.cpp
 //  TriviaGame
 //
-//
+// This File holds all of our functions to play the game as well as main which runs it
+// Functions are
+    // Game Rules:
+        // Lists the rules of the game
+    // Game Type:
+        // Asks the user how many players are playing the game
+        // returns that number
+    // Create Category:
+        // Hard coded with our current available categories and location of files
+        // creates with in category class for easy access
+        // Adds category to game.cats
+    // Create Question:
+        // For every category, opens file and creates questions in that category
+        // Adds questions to that category for easy accesss
+    // Print Categories:
+       // Prints the name of the current categores to console
+    // Choose Categories:
+      // Asks Current Player to choose category for question
+      // returns integer of category location in Categories vector
+   // Add Players:
+      // Takes game.number_of_players and creates that number of players for the game
+      // Adds players to game.players vector for easier access
+   // Player Alive:
+      // Checks to see if any players are currently alive in the game
+   // resetQuestionsAsked:
+     // Marks questions as not asked yet
+     // Called if all questions have been asked or at beginning of game
+   // Ask Qeustion:
+     // Finds next unasked question in that category
+     // Asks player that question
+     // Shuffles available answers using current time as seed
+     // Lists Possible answer with number
+     // Asks player to choose number of correct answer
+     // Checks if its write or wrong sends to HandleAnswer
+   // Handle Answer:
+     // Lets player know if they got it right or wrong
+     // Adds point or subtracts life by calling player class method
+   // CheckIFHighScoreIsBeaten:
+     // Compares highest player score to high score
+     // Lets player know if they beat the score
+  // FinalGameCalculations:
+    // Lists each player and their score
+    // If multiplayer lets them know who the winner is
+  // PlayGame:
+    // Loops through alive players and ask questions
+    // Once no one is alive calls final game calculations
+    // Calls NewGame to see if user wants to play again or not
+  // StartGame:
+    // Fills in classes with initial information so game can be played
+    // Ends with play game
+  // NewGame:
+    // Called at the end of Play Game to see if player wants to play another game or quit
+
 
 #include <iostream>
 #include <vector>
@@ -15,8 +67,6 @@
 
 using namespace std;
 // Globals
-vector<Categories> cats;
-vector<Player> game_players;
 Game g;
 HighScore h;
 
@@ -41,21 +91,21 @@ int GameType(){
 
 void CreateCategories(){
     // Creates categories in class and adds them to global vector
-    Categories music = Categories("Music", g, "moviequestions.csv");
+    Categories music = Categories("Movies", g, "moviequestions.csv");
     Categories sports = Categories("Sports", g, "sportsquestions.csv");
     
-    cats.push_back(music);
-    cats.push_back(sports);
+    g.cats.push_back(music);
+    g.cats.push_back(sports);
 }
 void CreateQuestions(){
     // Gets questions from csvs and adds them to the questions class and the appropriate category class
-    for (int i = 0; i < cats.size(); i++) {
+    for (int i = 0; i < g.cats.size(); i++) {
         vector<vector<string>> qs;
         vector<string> current_row;
         string line;
         string item;
         
-        fstream file (cats.at(i).file_location);
+        fstream file (g.cats.at(i).file_location);
         if (file.is_open()) {
             while (getline(file, line)) {
                 current_row.clear();
@@ -71,22 +121,22 @@ void CreateQuestions(){
         
         for (int j = 0; j < qs.size(); j++) {
             Question new_q;
-            new_q.category_name = cats[i].category;
+            new_q.category_name = g.cats[i].category;
             new_q.asked = false;
             new_q.question = qs[j][0];
             new_q.correct_answer =qs[j][1];
             new_q.answer2 =qs[j][2];
             new_q.answer3 =qs[j][3];
             new_q.answer4 =qs[j][4];
-            cats[i].addToCategory(new_q);
+            g.cats[i].addToCategory(new_q);
         }
     } // Ends category for loop
 }
 void PrintCategories(){
     // Print categories for player
     cout << "The Categories are: " << endl;
-    for (int i = 0; i < cats.size(); i++) {
-        cout << to_string(i) << ": " << cats.at(i).category << endl;
+    for (int i = 0; i < g.cats.size(); i++) {
+        cout << to_string(i) << ": " << g.cats.at(i).category << endl;
     }
 }
 
@@ -101,13 +151,13 @@ int ChooseCategories(){
 void AddPlayers() {
     // Dynamically adds players based on input
     // Default name is Player + number
-    game_players.resize(g.number_of_players);
+    g.players.resize(g.number_of_players);
     for(int i = 0; i < g.number_of_players; i++) {
     //Create each player for the game
         string p_name = "Player " + to_string(i + 1);
-        game_players[i].name = p_name;
-        game_players[i].current_game = g;
-        game_players[i].lives = 3;
+        g.players[i].name = p_name;
+        g.players[i].current_game = g;
+        g.players[i].lives = 3;
     }
 }
 
@@ -115,7 +165,7 @@ bool PlayerAlive () {
     // Checkst to see if any players have lives left
     bool alive = false;
     for(int i = 0; i < g.number_of_players; i++){
-        if ( game_players.at(i).lives > 0 ) {
+        if ( g.players.at(i).lives > 0 ) {
             alive = true;
         }
     }
@@ -123,8 +173,8 @@ bool PlayerAlive () {
 };
 
 void resetQuestionsAsked(int current_cat){
-    for (int i = 0; i < cats[current_cat].questions.size(); i++) {
-        cats[current_cat].questions[i].asked = false;
+    for (int i = 0; i < g.cats[current_cat].questions.size(); i++) {
+        g.cats[current_cat].questions[i].asked = false;
     }
 }
 
@@ -138,8 +188,8 @@ bool AskQuestion(int current_cat){
     vector<string> options;
     
     // Get next un-asked question
-    for (int i = 0; i < cats[current_cat].questions.size(); i++) {
-        if(cats[current_cat].questions[i].asked == false) {
+    for (int i = 0; i < g.cats[current_cat].questions.size(); i++) {
+        if(g.cats[current_cat].questions[i].asked == false) {
             rand = i;
             found_question = true;
             break;
@@ -152,13 +202,13 @@ bool AskQuestion(int current_cat){
         // Since all have been asked reseting
         resetQuestionsAsked(current_cat);
     }
-    options.push_back(cats[current_cat].questions[rand].correct_answer);
-    options.push_back(cats[current_cat].questions[rand].answer2);
-    options.push_back(cats[current_cat].questions[rand].answer3);
-    options.push_back(cats[current_cat].questions[rand].answer4);
+    options.push_back(g.cats[current_cat].questions[rand].correct_answer);
+    options.push_back(g.cats[current_cat].questions[rand].answer2);
+    options.push_back(g.cats[current_cat].questions[rand].answer3);
+    options.push_back(g.cats[current_cat].questions[rand].answer4);
     
     // Actually asking question
-    cout << cats[current_cat].questions[rand].question << endl;
+    cout << g.cats[current_cat].questions[rand].question << endl;
     
     // Shuffle Answers
     auto random = default_random_engine {};
@@ -172,13 +222,13 @@ bool AskQuestion(int current_cat){
     cout << "Please select an answer by number: " << endl;
     cin >> selected_answer;
         
-    if (options[selected_answer] == cats[current_cat].questions[rand].correct_answer ) {
+    if (options[selected_answer] == g.cats[current_cat].questions[rand].correct_answer ) {
         correct = true;
     } else {
             correct = false;
     }
         
-    cats[current_cat].questions[rand].asked = true;
+    g.cats[current_cat].questions[rand].asked = true;
         
     return correct;
 }
@@ -196,8 +246,8 @@ void HandleAnswer(bool correct, Player &player) {
 }
 
 void CheckIFHighScoreIsBeaten(int winning_player){
-    if (h.high_score < game_players.at(winning_player).total_score) {
-        h.high_score = game_players.at(winning_player).total_score;
+    if (h.high_score < g.players.at(winning_player).total_score) {
+        h.high_score = g.players.at(winning_player).total_score;
         cout << " You beat the high score!" << endl;
         cout << "Please enter your name: " << endl;
         cin >> h.player_name;
@@ -208,15 +258,15 @@ void FinalGameCalculations(){
     int highest_score = 0;
     int winning_player = 0;
     for(int i = 0; i < g.number_of_players; i++){
-        cout << game_players.at(i).name << "'s final score is " << game_players.at(i).total_score << endl;
-        if (game_players.at(i).total_score > highest_score){
-            highest_score = game_players.at(i).total_score;
+        cout << g.players.at(i).name << "'s final score is " << g.players.at(i).total_score << endl;
+        if (g.players.at(i).total_score > highest_score){
+            highest_score = g.players.at(i).total_score;
             winning_player = i;
         }
     }
     
     if (g.number_of_players > 1) {
-        cout << "Congats " << game_players.at(winning_player).name << "You Won!" << endl;
+        cout << "Congats " << g.players.at(winning_player).name << " You Won!" << endl;
     }
     
     CheckIFHighScoreIsBeaten(winning_player);
@@ -227,12 +277,12 @@ void PlayGame() {
     while (PlayerAlive()) {
         for(int i = 0; i < g.number_of_players; i++){
             // Goes through each player and asks question untill they lose 3 lives than goes to next player
-            if (game_players.at(i).lives > 0) {
+            if (g.players.at(i).lives > 0) {
                 cout << endl;
                 PrintCategories();
-                cout << game_players.at(i).name << " please choose a category by number: " << endl;
+                cout << g.players.at(i).name << " please choose a category by number: " << endl;
                 bool answered_correct = AskQuestion(ChooseCategories()); // Asks Player to choose a category and than pulls a random question
-                HandleAnswer(answered_correct, game_players[i]);
+                HandleAnswer(answered_correct, g.players[i]);
             }
         }
     }
--- TriviaGame/TriviaGame/TriviaHeader.hpp
+++ TriviaGame/TriviaGame/TriviaHeader.hpp
@@ -2,7 +2,7 @@
 //  TriviaHeader.hpp
 //  TriviaGame
 //
-//
+// This file declares the High Score, Game, Questions, Categories and Player classes for our Trivia Game.
 
 #ifndef TriviaHeader_hpp
 #define TriviaHeader_hpp
@@ -13,22 +13,26 @@
 
 using namespace std;
 
-void NewGame();
+void NewGame(); // Declaring here so that where it is located in the Trivia game does not matter
+class Player;
+class Categories;
 
 class HighScore {
+    // Records current high score in this game
 public:
     int high_score = 0;
     string player_name;
 };
 
 class Game {
+    // Creates instance of trivia game
 public:
     int number_of_players;
     int total_questions;
-    
+    vector<Player> players; // Collection of current players for easier access of players
+    vector<Categories> cats; // Collection of current categories for easy access to questions by category
     Game(int n){
-        number_of_players = n;
-        total_questions = 5;
+        number_of_players = n; // Use number of players to loop through current players for question asking and scoring
     }
     Game(){
         
@@ -36,6 +40,7 @@ public:
 };
 
 class Question {
+    // Filled from our csv files, this creates question objects with categories, correct answers and wrong answers.
 public:
     string category_name;
     string question;
@@ -50,11 +55,12 @@ public:
 };
 
 class Categories {
+    // Creates our categories which our questions fall into
 public:
     string category;
     string file_location;
     Game game;
-    vector<Question> questions;
+    vector<Question> questions; // Creates a collection of questions that are in this category
  
     void addToCategory(Question q){
         questions.push_back(q);
@@ -63,16 +69,14 @@ public:
         return 0;
     }
     Categories(string c, Game g, string f){
-        category = c;
-        game = g;
-        file_location = f;
+        category = c; // Category Name
+        game = g; // Current Game
+        file_location = f; // Location of the file in our code base
     }
     
 };
 
 class Player {
-private:
-    static vector<Player*> plyrList;
 public:
     string name;
     float total_score = 0;
@@ -80,18 +84,16 @@ public:
     Game current_game;
   
     void question_correct(){
+        // When anwering correctly adds one point to score
         total_score += 1;
     }
     void question_incorrect(){
-        lives -= 1;
+        lives -= 1; // Loses a live for each incorrect answer
     }
-    static vector<Player*> getAllPlayers(){
-      return plyrList;
-     }
     Player(string n, Game g ) {
       name = n;
-     current_game = g;
-        lives = 3;
+      current_game = g;
+      lives = 3; // All Players start with three lives
      }
     Player(){
         
